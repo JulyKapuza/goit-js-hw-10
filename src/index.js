955601be7bbe7d1dd15cde4948e281a20fce696b
Juliia Kapuza <105514086+JulyKapuza@users.noreply.github.com>
2022-09-22 14:18:59 +0300

    flag

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    searchBox: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+}
+
+
+fetch('https://restcountries.com/v3.1/name/peru')
+    .then(response => {
+    return response.json();
+    })
+//     .then(countries => {
+//     console.log(countries)
+// })
+    .then(renderCountryList)
+    .catch(error => {
+    console.log(error)
+})
+
+function renderCountryList(countries) {
+  const markup = countries
+    .map((country) => {
+        return `<li>
+      <svg><use href="${country.flags.svg}"></use>
+            </svg>
+          <p> ${country.name.common}</p>
+         </li>`;
+    })
+        .join("");
+    
+  refs.countryList.innerHTML = markup;
+}
